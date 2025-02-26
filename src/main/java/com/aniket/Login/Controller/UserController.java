0bbd8e914f10aea8a4ed9f863e0575330ab52082
aniket0880 <aabaravkar456@gmail.com>
2025-02-26 22:10:20 +0530

    Updated readme file

--- src/main/java/com/aniket/Login/Controller/UserController.java
+++ src/main/java/com/aniket/Login/Controller/UserController.java
@@ -14,26 +14,53 @@ public class UserController {
     @Autowired
     UserService service;
 
+    /*
+       Handles User Registration
+       Can be used by the user without authentication
+       User has to send a json file with
+       Username -: "aniket"
+       Email -: "aniketbaravkar@gmail.com"
+       Password -: "password"
+    */
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody Users user){
         return service.register(user);
     }
 
+    /*
+        Deletes the current user if he
+        wish to be delete his account
+    */
     @GetMapping("/delete")
     public ResponseEntity<String> delete(){
         return service.delete();
     }
 
+    /*
+       Edits the user details Username or email
+       User has to send the Json file with what he wants to edit
+       "Username" : "username"
+       "Email" : "email"
+    */
     @PostMapping("/edit")
     public ResponseEntity<String> edit(@RequestBody UsersWrapper user){
         return service.editUser(user);
     }
 
+    /*
+        Returns the user details username or email
+    */
     @GetMapping("/userDetails")
     public ResponseEntity<UsersWrapper> userControllerResponseEntity(){
         return service.userDetails();
     }
 
+    /*
+       Edits the password of the user
+       User has to send the Json file with the new password
+       "currentPassword" : "password"
+       "newPassword" : "newpassword"
+    */
     @PostMapping("/editPassword")
     public ResponseEntity<String> editPassword(@RequestBody PasswordWrapper password){
         return service.editPassword(password);
